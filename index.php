5fdc770b720c47ce81fd91fc8f76a2dfd001724e
pavalex <Alex.P79@mail.ru>
2022-02-05 13:27:04 +0800

    Условие

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
-
-$user_name = ''; // укажите здесь ваше имя
+$user_avatar = 'img/avatar.jpg';
+$user_name = 'Александр'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -28,7 +28,23 @@ $user_name = ''; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
+        <?php if ($is_auth): ?>
+            <div class="user-menu__image">
+                <img src="<?=$user_avatar; ?>" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+                <p><?=$user_name; ?></p>
+            </div>
+        <?php else: ?>
+            <ul class="user-menu__list">
+                <li class="user-menu__item">
+                    <a href="#">Регистрация</a>
+                </li>
+                <li class="user-menu__item">
+                    <a href="#">Вход</a>
+                </li>
+            </ul>
+        <?php endif; ?>
 
         </nav>
     </div>
